fcf7fa6aa374990e8ccb6ff0f7132513a5aecff1
Govardhan8 <47979981+Govardhan8@users.noreply.github.com>
2021-09-23 18:59:10 +0530

    Update Question2.js

--- DailyTasks/Day5/Question2.js
+++ DailyTasks/Day5/Question2.js
@@ -26,3 +26,37 @@ const sum = (arr) => {
 	console.log(`The sum of the elements is ${result}`)
 }
 sum([1, 2, 3, 4, 5])
+
+//Return all the prime numbers in an array
+const primeNumbers = (arr) => {
+	let result = arr.filter((a) => {
+		let flag = false
+		if (a > 2) {
+			for (let i = 2; i <= a / 2; i++) {
+				if (a % i === 0) {
+					flag = true
+					break
+				}
+			}
+		} else {
+			flag = true
+		}
+		return flag ? '' : a
+	})
+	console.log('Prime numbers are')
+	console.log(...result)
+	return result
+}
+primeNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 11])
+
+//Return all the palindromes in an array
+const palindromes = (arr) => {
+	var result = arr.filter((a) => {
+		b = a.split('').reverse().join('')
+		return a === b
+	})
+	console.log('The palindromes in the array are')
+	console.log(result)
+	return result
+}
+palindromes(['racecar', 'malayalam', 'tamil'])
