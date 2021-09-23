af85f0c5f70bb518b8fd5cd44951f3ef9c642155
Govardhan8 <47979981+Govardhan8@users.noreply.github.com>
2021-09-23 18:51:08 +0530

    Update question1.js

--- DailyTasks/Day5/question1.js
+++ DailyTasks/Day5/question1.js
@@ -33,7 +33,6 @@
 //Return all the prime numbers in an array
 ;(function () {
 	var arr = [1, 2, 3, 5, 6, 7, 8, 9, 10]
-	//var flag = true
 	var result = []
 	result = arr.filter((a) => {
 		let flag = false
