42212cd863eb6fcaa39fcd392e8c1d494755a789
Govardhan8 <47979981+Govardhan8@users.noreply.github.com>
2021-10-10 17:50:43 +0530

    Create index.js

---
+++ DailyTasks/Day16/task2/index.js
@@ -0,0 +1,130 @@
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="style.css" />
+		<title>Event 2</title>
+	</head>
+	<body>
+		<form action="submit" class="form-container" onsubmit="handleSubmit()">
+			<section class="section1">
+				<input type="search" placeholder="SEARCH EVENTS" />
+			</section>
+			<section class="section2">
+				<div class="event-container">
+					<h2 class="event-header">Event 1</h2>
+					<div class="image-container">
+						<img
+							src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfOYCQFxIpjvmaou1u6F0tGPHhlMzhFbx47w&usqp=CAU"
+							alt="stage mic "
+						/>
+					</div>
+					<div class="desc-container">
+						<p class="date">23-May-2020</p>
+						<p class="seats">Seats Available: 23</p>
+						<button class="book-button">Book Now</button>
+					</div>
+				</div>
+				<div class="event-container">
+					<h2 class="event-header">Event 2</h2>
+					<div class="image-container">
+						<img
+							src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfOYCQFxIpjvmaou1u6F0tGPHhlMzhFbx47w&usqp=CAU"
+							alt="stage mic "
+						/>
+					</div>
+					<div class="desc-container">
+						<p class="date">23-May-2020</p>
+						<p class="seats">Seats Available: 23</p>
+						<button class="book-button">Book Now</button>
+					</div>
+				</div>
+				<div class="event-container">
+					<h2 class="event-header">Event 3</h2>
+					<div class="image-container">
+						<img
+							src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfOYCQFxIpjvmaou1u6F0tGPHhlMzhFbx47w&usqp=CAU"
+							alt="stage mic "
+						/>
+					</div>
+					<div class="desc-container">
+						<p class="date">23-May-2020</p>
+						<p class="seats">Seats Available: 23</p>
+						<button class="book-button">Book Now</button>
+					</div>
+				</div>
+				<div class="event-container">
+					<h2 class="event-header">Event 4</h2>
+					<div class="image-container">
+						<img
+							src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfOYCQFxIpjvmaou1u6F0tGPHhlMzhFbx47w&usqp=CAU"
+							alt="stage mic "
+						/>
+					</div>
+					<div class="desc-container">
+						<p class="date">23-May-2020</p>
+						<p class="seats">Seats Available: 23</p>
+						<button class="book-button">Book Now</button>
+					</div>
+				</div>
+				<div class="event-container">
+					<h2 class="event-header">Event 5</h2>
+					<div class="image-container">
+						<img
+							src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfOYCQFxIpjvmaou1u6F0tGPHhlMzhFbx47w&usqp=CAU"
+							alt="stage mic "
+						/>
+					</div>
+					<div class="desc-container">
+						<p class="date">23-May-2020</p>
+						<p class="seats">Seats Available: 23</p>
+						<button class="book-button">Book Now</button>
+					</div>
+				</div>
+				<div class="event-container">
+					<h2 class="event-header">Event 6</h2>
+					<div class="image-container">
+						<img
+							src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfOYCQFxIpjvmaou1u6F0tGPHhlMzhFbx47w&usqp=CAU"
+							alt="stage mic "
+						/>
+					</div>
+					<div class="desc-container">
+						<p class="date">23-May-2020</p>
+						<p class="seats">Seats Available: 23</p>
+						<button class="book-button">Book Now</button>
+					</div>
+				</div>
+				<div class="event-container">
+					<h2 class="event-header">Event 7</h2>
+					<div class="image-container">
+						<img
+							src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfOYCQFxIpjvmaou1u6F0tGPHhlMzhFbx47w&usqp=CAU"
+							alt="stage mic "
+						/>
+					</div>
+					<div class="desc-container">
+						<p class="date">23-May-2020</p>
+						<p class="seats">Seats Available: 23</p>
+						<button class="book-button">Book Now</button>
+					</div>
+				</div>
+				<div class="event-container">
+					<h2 class="event-header">Event 8</h2>
+					<div class="image-container">
+						<img
+							src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfOYCQFxIpjvmaou1u6F0tGPHhlMzhFbx47w&usqp=CAU"
+							alt="stage mic "
+						/>
+					</div>
+					<div class="desc-container">
+						<p class="date">23-May-2020</p>
+						<p class="seats">Seats Available: 23</p>
+						<button class="book-button">Book Now</button>
+					</div>
+				</div>
+			</section>
+		</form>
+	</body>
+</html>
