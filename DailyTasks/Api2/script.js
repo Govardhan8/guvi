d74d37cd3f05bea7c4821060a4dd3cdbcdb49895
Govardhan8 <47979981+Govardhan8@users.noreply.github.com>
2021-11-07 20:13:41 +0530

    Add files via upload

---
+++ DailyTasks/Api2/script.js
@@ -0,0 +1,38 @@
+const fetchData = async () => {
+	// Fetching the data from api
+	const data = await fetch('https://rickandmortyapi.com/api/character/')
+	const jsonData = await data.json()
+	console.log(jsonData.results)
+	//Calling the function to render Data in html
+	render(jsonData.results)
+}
+
+//Rendering the fetched data in DOM
+const render = (data) => {
+	//Getting the container element from document object/DOM
+	const container = document.querySelector('.container')
+	let images = ''
+	data.forEach(({ name, image, status, species, origin, location }) => {
+		//Dynamically converting the api data into html content
+		const className = status === 'Alive' ? 'green' : 'red'
+		images += `
+        <article class="imageContainer">
+            <img src=${image} alt=${name} class="image"/>
+            <div class="details">
+                <h3 class="name">${name}</h3>
+                <h4 class="status ${className}">${status} - ${species}</h4>
+                <p class="location"><span>Last known Location:</span>${location.name}</p>
+                <p class="location"><span>First seen in:</span>${origin.name}</p>
+            </div>
+        </article>
+        `
+	})
+
+	//Adding the image Containers into the container
+	container.innerHTML = `
+    ${images}
+    `
+}
+
+//Calling the function to fetch data from api
+fetchData()
